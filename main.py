3563c64a95b7578a9dec0e6383c772472a006913
Inioluwa Davies <inioluwadavies@my.unt.edu>
2022-07-19 19:33:55 +0000

    code now displays images from unsplash api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, session
 import requests
 from random import randint
 from flask_sqlalchemy import SQLAlchemy
@@ -15,9 +15,19 @@ def request_images(theme):
     # Make request to unsplash api for a list of 9 images
     headers = {'Authorization': 'Client-ID {token}'.format(token=CLIENT_ID), 'Accept-Version': 'v1'}
     BASE_URL = 'https://api.unsplash.com/'
-    PARAMS = {'query': theme, 'orientation': 'landscape', 'count': 1}
+    PARAMS = {'query': theme, 'orientation': 'landscape', 'count': 9}
     r = requests.get(BASE_URL + '/photos/random', params = PARAMS, headers=headers)
     data = r.json()
+    return data
+
+
+def display_images(images):
+    image_urls =[]
+    for image in images:
+        image_urls.append(image['urls']['regular'])
+        print(image['urls']['regular'])
+    return image_urls
+
 
 #Create a flask app for the website
 app = Flask(__name__)
@@ -32,17 +42,17 @@ app.config['SECRET_KEY'] = secrets.token_hex(16)
 def home():
     search_form = Search_Form() #Flask Form object
     if search_form.validate_on_submit(): # checks if search entry is valid
-
-        request_images(search_form.search.data) #makes request to the API based on the user's inputed search
-
+        session['search_term'] = search_form.search.data 
         return redirect(url_for('board')) # if so - send to board page
     return render_template('home.html', form=search_form)
 
 
 # Inspiration board webpage function
-@app.route("/board", methods=['GET', 'POST'])
+@app.route("/board", methods=['GET'])
 def board():
-    return render_template('board.html')
+    image_data = request_images(session.get("search_term")) #makes request to the API based on the user's inputed search
+    image_urls = display_images(image_data)
+    return render_template('board.html', images=image_urls)
 
 
 if __name__ == '__main__':
