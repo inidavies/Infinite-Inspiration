223db1cb3c3b59fcdf05952227e635e7f43d8bbd
Emily Berger <eberge11@jhu.edu>
2022-07-19 18:17:18 +0000

    packaged unsplash interactions into images folder
    Co-authored-by: Marianela Luna-Torrado <mlunatorrado@gmail.com>

--- images/unsplash.py
+++ images/unsplash.py
@@ -5,14 +5,17 @@ CLIENT_ID = os.environ.get('UNSPLASH_CLIENT_ID')
 CLIENT_SECRET = os.environ.get('UNSPLASH_CLIENT_SECRET')
 BASE_URL = 'https://api.unsplash.com/'
 
+# defining import * for the __init__.py file (to obscure other functions)
+__all__ = ['get_images']
+
 # Call this function
 def get_images(theme):
-    theme = input('Enter your desired theme: ')
+    # theme = input('Enter your desired theme: ')
     response = request_images(theme)
     processed_info = process_json(response)
     return processed_info
 
-# Don't call any of these
+# Description: chandles 
 def request_images(theme):
     headers = {'Authorization': 'Client-ID {token}'.format(token=CLIENT_ID), 'Accept-Version': 'v1'}
     PARAMS = {'query': theme, 'orientation': 'landscape', 'count': 1}
