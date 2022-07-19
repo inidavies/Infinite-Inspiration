0ef2027e0ff0bb86fe8a99557a448959f8ba46cc
Inioluwa Davies <inioluwadavies@my.unt.edu>
2022-07-19 15:30:00 +0000

    converted html search form to a flask form using python and redirect to board page on submit

---
+++ forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class Search_Form(FlaskForm):
+    search = StringField('Search a theme',
+                           validators=[DataRequired(), Length(min=2, max=20)])
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from random import randint
 from flask_sqlalchemy import SQLAlchemy
 import secrets
 from flask_behind_proxy import FlaskBehindProxy
+from forms import Search_Form
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)
@@ -14,7 +15,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    search_form = Search_Form() #Flask Form object
+    if search_form.validate_on_submit(): # checks if entries are valid
+        return redirect(url_for('board')) # if so - send to board page
+    return render_template('home.html', form=search_form)
 
   
 @app.route("/board", methods=['GET', 'POST'])
