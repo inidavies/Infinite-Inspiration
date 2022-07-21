6147ff2e5859ed915b8871001845961508c40f5a
Emily Berger <eberge11@jhu.edu>
2022-07-21 15:46:28 +0000

    trying to figure out why its timing out

--- tests/unittests.py
+++ tests/unittests.py
@@ -1,6 +1,7 @@
 import unittest
 import requests
 import sys
+import time
 
 sys.path.append('../Infinite-Inspiration')
 from color import background_color
@@ -11,36 +12,50 @@ class ImageUnitTests(unittest.TestCase):
 
     def testIsDictionary(self):
         bing_data = background_color("https://th.bing.com/th/id/OIP.LIyeXFdvM83UkH_jNud3zwHaE5?pid=ImgDet&rs=1")
+        time.sleep(6)
         self.assertIsInstance(bing_data, dict)
 
     def testIsDictionaryUnsplash(self):
         unsplash_data = background_color("https://source.unsplash.com/gySMaocSdqs/w=600")
+        time.sleep(6)
         self.assertIsInstance(unsplash_data, dict)
     
     def testIsDictionaryShortened(self):
         shortened_url_data = background_color("https://tinyurl.com/2area6w6")
+        time.sleep(6)
         self.assertIsInstance(shortened_url_data, dict)
 
     def testLengthOfDictionary(self):
         unsplash_data = background_color("https://source.unsplash.com/gySMaocSdqs/w=600")
         bing_data = background_color("https://th.bing.com/th/id/OIP.LIyeXFdvM83UkH_jNud3zwHaE5?pid=ImgDet&rs=1")
+        time.sleep(6)
+        # have to add this so it doesn't break
+        self.assertIsInstance(unsplash_data, dict)
+        self.assertIsInstance(bing_data, dict)
         self.assertEqual(len(unsplash_data), 2)
         self.assertEqual(len(bing_data), 2)
 
     def testLightColorResponseHSLLigtnessUnsplash(self):
         unsplash_data = background_color("https://source.unsplash.com/gySMaocSdqs/w=600")
+        time.sleep(6)
+        self.assertIsInstance(unsplash_data, dict)
         light = unsplash_data['light'][1:]
         response = requests.get(the_color_api_id_url + light).json()['hsl']['l']
         self.assertGreaterEqual(response, 75)
 
     def testLightColorResponseHSLLightnessBing(self):
         bing_data = background_color("https://th.bing.com/th/id/OIP.LIyeXFdvM83UkH_jNud3zwHaE5?pid=ImgDet&rs=1")
+        time.sleep(6)
+        print(bing_data)
+        self.assertIsInstance(bing_data, dict)
         light = bing_data['light'][1:]
         response = requests.get(the_color_api_id_url + light).json()['hsl']['l']
         self.assertGreaterEqual(response, 75)
 
     def testLightColorResponseHSLLightnessShortened(self):
         shortened_url_data = background_color("https://tinyurl.com/2area6w6")
+        time.sleep(6)
+        self.assertIsInstance(shortened_url_data, dict)
         light = shortened_url_data['light'][1:]
         response = requests.get(the_color_api_id_url + light).json()['hsl']['l']
         self.assertGreaterEqual(response, 75)
