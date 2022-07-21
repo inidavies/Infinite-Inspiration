cb386f0e94b2766b1a74c746b625f18ad9da511e
Inioluwa Davies <inioluwadavies@my.unt.edu>
2022-07-21 20:09:55 +0000

    refresh works, there's a link to history, now going to add flask code for that

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from random import randint
 from flask_sqlalchemy import SQLAlchemy
 import secrets
 from flask_behind_proxy import FlaskBehindProxy
-from forms import Search_Form, Refresh_Click
+from forms import Search_Form
 import os
 from color import background_color
 from images import get_images
+from dbtable import previous_boards
 
 
 def get_urls(images):
@@ -20,7 +21,8 @@ def get_credit(images, spotlight):
     image_author = []
     for image in images:
         if spotlight == image['regular_url']:
-            image_urls.append([image['photographer'], image['photographer_profile']])
+            image_author.append(image['photographer'])
+            image_author.append(image['photographer_profile'])
             break
     return image_author
 
@@ -29,6 +31,7 @@ def search_image(form):
     if form.validate_on_submit(): # checks if search entry is valid
         # Makes request to the API based on the user's inputed search; better implementation with a database, move to home page
         image_data = get_images(form.search.data)
+        session["search_term"] = form.search.data
         session['search_results'] = image_data
         return True
 
@@ -57,20 +60,22 @@ def home():
     #Flask Form object
     search_form = Search_Form()
     if search_image(search_form) == True:
-        return redirect(url_for('board')) # if so - send to board page
+        return redirect(url_for('board', search_term=search_form.search.data)) # if so - send to board page
     
     return render_template('home.html', form=search_form)
 
 
 # Inspiration board webpage function
-@app.route("/board", methods=['GET', 'POST'])
-def board():
-    # Flask Form object
+@app.route("/board/<search_term>", methods=['GET', 'POST'])
+def board(search_term):
+    #Search bar
+    #Flask Form object
     search_form = Search_Form()
+    if search_image(search_form) == True:
+        return redirect(url_for('board', search_term=search_form.search.data)) # if so - send to board page
 
-    # Makes request to the API based on the user's inputed search; better implementation with a database  
+    # Makes request to the API based on the user's inputed search
     image_data = session['search_results']
-    
     # Gets a list of the requested images (url)
     image_urls = get_urls(image_data)
 
@@ -80,36 +85,65 @@ def board():
     bgcolor = page_colors['light']
     navcolor = page_colors['dark']
     
-    # When image is clicked, redirect to webpage displaying credits to the author
     if request.method == 'POST':
         image_click_url = request.form.get('submit')
-        session['image_click_url'] = image_click_url 
-        # Changes the page color scheme based on the main image color
-        page_colors = background_color(image_click_url)
-        session['bglight'] = page_colors["light"]
-        session['bgdark'] = page_colors["dark"]
-        return redirect(url_for("credit")) # Go to the credit page
+        if image_click_url == "refresh":
+            # Refreshes the webpage with the current search
+            image_data = get_images(search_term)
+            session['search_results'] = image_data
+            image_urls = get_urls(image_data)
+            main_index = get_main(image_urls)
+            page_colors = background_color(image_urls[main_index])
+            bgcolor = page_colors['light']
+            navcolor = page_colors['dark']
+        else:
+            # When image is clicked, redirect to webpage displaying credits to the author
+            session['image_click_url'] = image_click_url 
+            # Changes the page color scheme based on the main image color
+            page_colors = background_color(image_click_url)
+            session['bglight'] = page_colors["light"]
+            session['bgdark'] = page_colors["dark"]
+            return redirect(url_for("credit")) # Go to the credit page
 
-    return render_template('board.html', form=search_form, images=image_urls, bgcolor = bgcolor, navcolor=navcolor, home=url_for("home"))
+    return render_template('board.html', form=search_form, images=image_urls, bgcolor = bgcolor, navcolor=navcolor, home=url_for("home"), faves=url_for("faves"))
 
 
 # Author credit webpage function
 @app.route("/credit", methods=['GET', 'POST'])
 def credit():
+    print(previous_boards())
     #Flask Form object
     search_form = Search_Form()
     if search_image(search_form) == True:
         return redirect(url_for('board')) # if so - send to board page
-
-    # Returns author information; better implementation with a database
-    # author_data = get_credit(session['image_click_url'])
     
     image_url = session['image_click_url']
+
+    # Returns author information
+    image_data = session['search_results']
+    author_data = get_credit(image_data, image_url)
+    author_name = author_data[0]
+    author_profile = author_data[1]
 
     bgcolor = session['bglight']
     navcolor = session['bgdark']
 
-    return render_template('credit.html',form=search_form, image=image_url, bgcolor = bgcolor, navcolor=navcolor)
+    return render_template('credit.html',form=search_form, image=image_url, bgcolor = bgcolor, navcolor=navcolor, a_profile=author_profile, a_name=author_name,  faves=url_for("faves"))
+
+@app.route("/favs", methods=['GET', 'POST'])
+def faves():
+    #Flask Form object
+    search_form = Search_Form()
+    if search_image(search_form) == True:
+        return redirect(url_for('board')) # if so - send to board page
+    
+    if request.method == 'POST':
+        # Sets session search results to the chosen previous search results
+        session['search_results'] = request.form.get('submit')
+        # Redirects to a page displaying that board
+        return redirect(url_for("board", search_term="past_board"))
+
+    return render_template('faves.html', form=search_form)
 
 
 if __name__ == '__main__':
