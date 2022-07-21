47e45b7d10779655e844b9ea3195714a4489ea49
Emily Berger <eberge11@jhu.edu>
2022-07-21 18:21:42 +0000

    redid some comments

--- color/imagga.py
+++ color/imagga.py
@@ -2,10 +2,11 @@ import os
 import random
 import requests
 
-# https://docs.imagga.com/#colors to get color from picture
-# https://www.thecolorapi.com/docs to get light enough version of the
-# color to be suitable for background
-# Important info: doesn't work on goo.gl urls but does work on shortened urls (tinyurls)
+"""
+https://docs.imagga.com/#colors to get color from picture
+https://www.thecolorapi.com/docs to get light enough version of the
+color to be suitable for background
+"""
 
 #defining import * for the __init__.py file (to obscure all other functions)
 __all__ = ['background_color']
@@ -16,11 +17,13 @@ imagga_api_secret = os.environ.get('IMAGGA_CLIENT_SECRET')
 imagga_color_url = 'https://api.imagga.com/v2/colors?image_url='
 the_color_api_scheme_url = 'https://www.thecolorapi.com/scheme?hex='
 
-# USE THIS TO CALL IN OTHER LOCATIONS
-# Description: driver for the imagga and The Color API interaction
-# Input: image url (from wherever you are currently working)
-# Output: Returns a dictionary with 2 elements: dark (darker color hex code)
-# and light (lighter color hex code) -1 if bad url
+"""
+Use this function to call from outside the package
+Description: driver for the imagga and The Color API interaction
+Input: image url (from wherever you are currently working)
+Output: Returns a dictionary with 2 elements: dark (darker color hex code)
+and light (lighter color hex code) -1 if bad url
+"""
 def background_color(img_url):
     response = get_json_response_imagga(img_url)
     if type(response) is not str:
@@ -40,10 +43,13 @@ def background_color(img_url):
     else:
         return -1
 
-# DO NOT USE ANY OF THE FUNCTIONS BELOW THIS LINE FOR ANYTHING OUTSIDE OF THIS SCRIPT
-# Description: handles the api call to imagga
-# Input: takes the image url as input
-# Output: returns the response in json format
+"""
+DO NOT USE ANY OF THE FUNCTIONS BELOW THIS LINE FOR ANYTHING OUTSIDE OF THIS SCRIPT
+Description: handles the api call to imagga
+Input: takes the image url as input
+Output: returns the response in json format
+"""
+
 def get_json_response_imagga(img_url):
     response = -1
     try:
@@ -56,9 +62,11 @@ def get_json_response_imagga(img_url):
             resp = str(response.status_code) + ': ' + str(response.reason)
             return resp
 
-# Description: gets up to 3 background colors and up to 3 foreground colors
-# Input: takes the json response as input
-# Output: returns a single array of non-duplicate colors
+"""
+Description: gets up to 3 background colors and up to 3 foreground colors
+Input: takes the json response as input
+Output: returns a single array of non-duplicate colors
+"""
 def process_json_response(json):
     unprocessed_background = json['colors']['background_colors']
     processed_background = []
@@ -73,9 +81,11 @@ def process_json_response(json):
 
     return eliminate_duplicates(processed_background, processed_foreground)
 
-# Description: eliminates duplicates from the two target arrays
-# Input: takes the foreground and background color arrays as input
-# Output: returns a single array with all colors from both that aren't duplicates
+"""
+Description: eliminates duplicates from the two target arrays
+Input: takes the foreground and background color arrays as input
+Output: returns a single array with all colors from both that aren't duplicates
+"""
 def eliminate_duplicates(background, foreground):
     to_return = foreground
     for color in background:
@@ -83,27 +93,33 @@ def eliminate_duplicates(background, foreground):
             to_return += [color]
     return to_return
 
-# Description: gets the target color from the array of options
-# Input: takes a single array of colors as input
-# Output: returns a random entry from the array
+"""
+Description: gets the target color from the array of options
+Input: takes a single array of colors as input
+Output: returns a random entry from the array
+"""
 def pick_color(color_options):
     length = len(color_options)
     index = random.randrange(0, length)
     return color_options[index]
 
-# Description: gets a color scheme based on the target color
-# Input: hex color code with hashtag
-# Output: json response from API
+"""
+Description: gets a color scheme based on the target color
+Input: hex color code with hashtag
+Output: json response from API
+"""
 def get_json_response_color_scheme(hex_color):
     #slicing the # off
     hex_color = hex_color[1:]
     response = requests.get(the_color_api_scheme_url + hex_color)
     return response.json()
 
-# Description: picks the color from the API- not the lightest but also not nearly the darkes
-# Input: unprocessed json response
-# Output: lightest hex color code if hsl is greater than 75, if it is less than, it returns the 
-# lighest color to be rerun (to get a lighter version of it)
+"""
+Description: picks the color from the API- not the lightest but also not nearly the darkes
+Input: unprocessed json response
+Output: lightest hex color code if hsl is greater than 75, if it is less than, it returns the 
+lighest color to be rerun (to get a lighter version of it)
+"""
 def process_json_response_color_scheme(response):
     colors = response['colors']
     returnable_colors = {}
