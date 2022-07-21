294cef29b61b99b6c48bcac25fb71ddc13a6408c
Emily Berger <eberge11@jhu.edu>
2022-07-21 15:39:20 +0000

    updated tests and yaml files

--- tests/unittests.py
+++ tests/unittests.py
@@ -8,36 +8,40 @@ from color import background_color
 the_color_api_id_url = 'https://www.thecolorapi.com/id?hex='
 
 class ImageUnitTests(unittest.TestCase):
-    def setUp(self):
-        self.unsplash_data = background_color("https://source.unsplash.com/gySMaocSdqs/w=600")
-        self.bing_data = background_color("https://th.bing.com/th/id/OIP.LIyeXFdvM83UkH_jNud3zwHaE5?pid=ImgDet&rs=1")
-        self.shortened_url_data = background_color("https://tinyurl.com/2area6w6")
 
     def testIsDictionary(self):
-        self.assertIsInstance(self.bing_data, dict)
+        bing_data = background_color("https://th.bing.com/th/id/OIP.LIyeXFdvM83UkH_jNud3zwHaE5?pid=ImgDet&rs=1")
+        self.assertIsInstance(bing_data, dict)
 
     def testIsDictionaryUnsplash(self):
-        self.assertIsInstance(self.unsplash_data, dict)
+        unsplash_data = background_color("https://source.unsplash.com/gySMaocSdqs/w=600")
+        self.assertIsInstance(unsplash_data, dict)
     
     def testIsDictionaryShortened(self):
-        self.assertIsInstance(self.shortened_url_data, dict)
+        shortened_url_data = background_color("https://tinyurl.com/2area6w6")
+        self.assertIsInstance(shortened_url_data, dict)
 
     def testLengthOfDictionary(self):
-        self.assertEqual(len(self.unsplash_data), 2)
-        self.assertEqual(len(self.bing_data), 2)
+        unsplash_data = background_color("https://source.unsplash.com/gySMaocSdqs/w=600")
+        bing_data = background_color("https://th.bing.com/th/id/OIP.LIyeXFdvM83UkH_jNud3zwHaE5?pid=ImgDet&rs=1")
+        self.assertEqual(len(unsplash_data), 2)
+        self.assertEqual(len(bing_data), 2)
 
     def testLightColorResponseHSLLigtnessUnsplash(self):
-        light = self.unsplash_data['light'][1:]
+        unsplash_data = background_color("https://source.unsplash.com/gySMaocSdqs/w=600")
+        light = unsplash_data['light'][1:]
         response = requests.get(the_color_api_id_url + light).json()['hsl']['l']
         self.assertGreaterEqual(response, 75)
 
     def testLightColorResponseHSLLightnessBing(self):
-        light = self.bing_data['light'][1:]
+        bing_data = background_color("https://th.bing.com/th/id/OIP.LIyeXFdvM83UkH_jNud3zwHaE5?pid=ImgDet&rs=1")
+        light = bing_data['light'][1:]
         response = requests.get(the_color_api_id_url + light).json()['hsl']['l']
         self.assertGreaterEqual(response, 75)
 
     def testLightColorResponseHSLLightnessShortened(self):
-        light = self.shortened_url_data['light'][1:]
+        shortened_url_data = background_color("https://tinyurl.com/2area6w6")
+        light = shortened_url_data['light'][1:]
         response = requests.get(the_color_api_id_url + light).json()['hsl']['l']
         self.assertGreaterEqual(response, 75)
     
