45e921b005a29d90ae24bf12a796f92a21e66654
Marianela Luna-Torrado <mlunatorrado@gmail.com>
2022-07-22 01:31:17 +0000

    commented code and fixed style mistakes

--- dbtable.py
+++ dbtable.py
@@ -5,13 +5,17 @@ import os
 
 
 def create_database():
+    """Creates database"""
     engine = db.create_engine("sqlite:///boards.db")
     return engine
+
 
 all_boards = []
 board_num = 0
 
+
 def create_table(engine, theme):
+    """Creates table with board information"""
     global board_num
     board_num += 1
     data = get_images(theme)
@@ -23,11 +27,14 @@ def create_table(engine, theme):
         return -1
     return data
 
+
 def previous_boards():
+    """Reverses all_boards"""
     all_boards_reversed = list(reversed(all_boards))
     return all_boards_reversed
 
 
 def delete_database(engine):
+    """Deletes database"""
     engine.dispose()
     os.remove('boards.db')
