6e19b812c8bef3a95140c000ca1e767391361416
Inioluwa Davies <inioluwadavies@my.unt.edu>
2022-07-22 01:58:50 +0000

    penultimate push, hopefully

--- dbtable.py
+++ dbtable.py
@@ -15,9 +15,12 @@ def create_table(engine, theme):
     global board_num #access board_num on line 15
     board_num += 1 #increment board num (key)
     data = get_images(theme) #get data (value)
-    all_boards.append(data) #create k-v pair and add to all_boards dict
-    df = pd.DataFrame(data) #create dataframe for board
-    df.to_sql(str(board_num), con=engine, if_exists='replace', index=False) #transform dataframe into table
+    if type(data) is list:
+        all_boards.append(data) #create k-v pair and add to all_boards dict
+        df = pd.DataFrame(data) #create dataframe for board
+        df.to_sql(str(board_num), con=engine, if_exists='replace', index=False) #transform dataframe into table
+    else:
+        return -1
     return data
 
 def previous_boards():
